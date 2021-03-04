475b74a75d55ee332a77d5a153518b27c79a9960
root <vibishygal@gmail.com>
2021-03-04 00:32:49 +0000

    modified params

--- tests/ixia/bgp/test_bgp_convergence.py
+++ tests/ixia/bgp/test_bgp_convergence.py
@@ -1,27 +1,12 @@
 from tests.common.ixia.ixia_fixtures import snappi_api
-from tests.common.ixia.ixia_fixtures import (
-    ixia_api_serv_ip, ixia_api_serv_port, tgen_ports)
+from tests.common.ixia.ixia_fixtures import (ixia_api_serv_ip, ixia_api_serv_port, tgen_ports)
 from files.helper import run_bgp_convergence_test
-from tests.common.fixtures.conn_graph_facts import (
-    conn_graph_facts, fanout_graph_facts)
+from tests.common.fixtures.conn_graph_facts import (conn_graph_facts, fanout_graph_facts)
 import pytest
 
 @pytest.mark.parametrize('multipath',[2])
 @pytest.mark.parametrize('convergence_test_iterations',[1])
-def test_bgp_convergence(snappi_api,
-                        duthost,
-                        tgen_ports,
-                        conn_graph_facts,
-                        fanout_graph_facts,
-                        multipath,
-                        convergence_test_iterations):
-
-
+def test_bgp_convergence(snappi_api,duthost,tgen_ports,conn_graph_facts,fanout_graph_facts,multipath,convergence_test_iterations):
     #convergence_test_iterations and multipath values can be modified as per user preference
-    run_bgp_convergence_test(snappi_api,
-                            duthost,
-                            tgen_ports,
-                            convergence_test_iterations,
-                            multipath)
+    run_bgp_convergence_test(snappi_api,duthost,tgen_ports,convergence_test_iterations,multipath)
     
-#this is just to check
